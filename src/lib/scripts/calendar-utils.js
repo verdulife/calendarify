1c90858a8866c3d010ef15f63c9bf654bae21ffb
verdulife <verdu@live.com>
2022-12-02 17:42:47 +0100

    can make calendars

---
+++ src/lib/scripts/calendar-utils.js
@@ -0,0 +1,62 @@
+export const yearMonths = 12;
+export const weekDays = 7;
+export const currentYear = new Date().getFullYear();
+
+export function getLastMonthDay(month, year) {
+	const nextMonth = new Date(`${month + 1}/1/${year}`);
+	const lastDay = new Date(nextMonth - 1).getDate();
+
+	return lastDay || 31;
+}
+
+export function getCalendarData(year, language) {
+	let yearData = [];
+
+	for (let m = 1; m <= yearMonths; m++) {
+		const currentMonth = new Date(`${m}/1/${year}`);
+		const lastMonthDay = getLastMonthDay(m, year);
+
+		let monthData = {
+			name: new Intl.DateTimeFormat(language, { month: 'long' }).format(currentMonth),
+			number: new Intl.DateTimeFormat(language, { month: 'numeric' }).format(currentMonth),
+			days: []
+		};
+
+		for (let d = 1; d <= lastMonthDay; d++) {
+			const currentDay = new Date(`${m}/${d}/${year}`);
+
+			const dayData = {
+				fullDate: new Intl.DateTimeFormat(language, { dateStyle: 'full' }).format(currentDay),
+				number: new Intl.DateTimeFormat(language, { day: 'numeric' }).format(currentDay),
+				weekday: {
+					number: currentDay.getDay(),
+					name: new Intl.DateTimeFormat(language, { weekday: 'long' }).format(currentDay)
+				},
+				month: {
+					number: new Intl.DateTimeFormat(language, { month: 'numeric' }).format(currentDay),
+					name: new Intl.DateTimeFormat(language, { month: 'long' }).format(currentDay)
+				},
+				year: new Intl.DateTimeFormat(language, { year: 'numeric' }).format(currentDay)
+			};
+
+			monthData.days = [...monthData.days, dayData];
+		}
+
+		yearData = [...yearData, monthData];
+	}
+
+	return yearData;
+}
+
+export function getWeekdays(language) {
+	const startDay = new Date('11/27/2022');
+	let weekdays = [];
+
+	for (let w = 0; w < weekDays; w++) {
+		startDay.setDate(startDay.getDate() + (w === 0 ? 0 : 1));
+		const weekday = new Intl.DateTimeFormat(language, { weekday: 'long' }).format(startDay);
+		weekdays = [...weekdays, weekday];
+	}
+
+	return weekdays;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,121 @@
 <script>
 	import { onMount } from 'svelte';
 	import { jsPDF } from 'jspdf';
-	import { getCalendarData, currentYear } from '$lib/scripts/getCalendarData';
+	import { getCalendarData, currentYear, getWeekdays } from '$lib/scripts/calendar-utils';
+	import { capitalize } from '$lib/scripts/text-utils';
 
 	const yearData = getCalendarData(currentYear, 'es-Es');
+	const weekdayList = getWeekdays('es-Es');
 	let src;
 
 	async function createPdf() {
-		console.log(yearData);
+		const pageWidth = 210;
+		const pageHeight = 297;
 
 		const doc = new jsPDF({
 			orientation: 'p',
 			unit: 'mm',
-			format: [210, 297]
+			format: [pageWidth, pageHeight],
+			putOnlyUsedFonts: true
 		});
 
-		doc
-			.setFontSize(30)
-			.text(
-				'esto es un texto ligeramente largo para hacer que se tenga de saltar la linea',
-				10,
-				10,
-				{
-					maxWidth: 210
-				}
-			);
+		const margin = 10;
+		const marginWidth = pageWidth - margin * 2;
+		const marginHeight = pageHeight - margin * 2;
 
-		const blob = doc.output('bloburi');
+		doc.stroke().rect(margin, margin, marginWidth, marginHeight);
+
+		const monthColLength = 3;
+		const monthRowLength = 4;
+		const monthWidth = marginWidth / monthColLength;
+		const monthHeight = marginHeight / monthRowLength;
+		const monthMargin = 5;
+		const monthMarginWidth = monthWidth - monthMargin * 2;
+		const monthMarginHeight = monthHeight - monthMargin * 2;
+
+		let monthPosX, monthPosY, monthContentX, monthContentY;
+		let monthIndex = 0;
+
+		for (let mr = 0; mr < monthRowLength; mr++) {
+			for (let mc = 0; mc < monthColLength; mc++) {
+				monthPosX = margin + monthWidth * mc;
+				monthPosY = margin + monthHeight * mr;
+				monthContentX = monthPosX + monthMargin;
+				monthContentY = monthPosY + monthMargin + 5;
+
+				doc.stroke().rect(monthPosX, monthPosY, monthWidth, monthHeight);
+
+				const { name, days } = yearData[monthIndex];
+
+				doc
+					.setFontSize(12)
+					.text(capitalize(name), monthContentX + monthMarginWidth / 2, monthContentY - 3, {
+						maxWidth: monthMarginWidth,
+						align: 'center',
+						baseline: 'bottom'
+					});
+
+				const dayColLength = 7;
+				const dayRowLength = 7;
+				const dayWidth = monthMarginWidth / dayColLength;
+				const dayHeight = monthMarginHeight / dayRowLength;
+				const dayMargin = 1;
+				const dayMarginWidth = dayWidth - dayMargin * 2;
+				const startWeekday = days[0].weekday.number;
+
+				let dayPosX, dayPosY, dayContentX, dayContentY;
+				let dayIndex = 0;
+
+				for (let dr = 0; dr < dayRowLength; dr++) {
+					for (let dc = 0; dc < dayColLength; dc++) {
+						dayPosX = monthContentX + dayWidth * dc;
+						dayPosY = monthContentY + dayHeight * dr;
+						dayContentX = dayPosX + dayMargin;
+						dayContentY = dayPosY + dayMargin;
+
+						doc.stroke().rect(dayPosX, dayPosY, dayWidth, dayHeight);
+
+						if (dr === 0) {
+							const wd = weekdayList[dc].substring(0, 2);
+							doc.setFontSize(8).text(capitalize(wd), dayContentX, dayContentY, {
+								maxWidth: dayMarginWidth,
+								baseline: 'top'
+							});
+
+							continue;
+						}
+
+						if (dr === 1 && dc < startWeekday) continue;
+
+						if (days[dayIndex]) {
+							doc.setFontSize(8).text(days[dayIndex].number, dayContentX, dayContentY, {
+								maxWidth: dayMarginWidth,
+								baseline: 'top'
+							});
+						} else {
+							doc.setFontSize(8).text('', dayContentX, dayContentY, {
+								maxWidth: dayMarginWidth,
+								baseline: 'top'
+							});
+						}
+
+						dayIndex++;
+					}
+				}
+
+				monthIndex++;
+			}
+		}
+
+		doc.setProperties({
+			title: 'Calendarify™',
+			subject: 'Calendarify™',
+			author: 'Calendarify™',
+			keywords: 'Calendarify™',
+			creator: 'Calendarify™'
+		});
+
+		const blob = doc.output('bloburi', 'filename');
 		src = blob + '#view=fit';
 	}
 
